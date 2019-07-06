e5ba377190eef0d23f0f4418394841c3b10d44fd
Andrew Emerick <aemerick11@gmail.com>
2019-07-06 12:46:35 -0400

    documentation

--- Enzo/ICs/write_enzo_ic.sh
+++ Enzo/ICs/write_enzo_ic.sh
@@ -1,14 +1,31 @@
 #!/bin/bash
 
+
+#
+# Simple script to execute ALL commands needed to
+# generate the SMAUG isolated galaxy (WLM) ICs
+# in the format needed to be read in for Enzo.
+#
+# This assumes you already have MakeDiskGalaxy compiled
+# and loadIC.c compiled. loadIC.c is an interpreter written
+# by and obtained from Chia-Yu Hu to parse the MakeDiskGalaxy
+# output that has been edited to conform to how Enzo needs the
+# data in ProblemType_Agora.
+#
+# Final data products are zipped together to be moved elsewhere
+# if needed (see last command)
+
 MDGNAME="WLM_1E7DM"
 MDGINPUT="input_$MGDNAME.txt"
-RUN="/home/aemerick/work/SMAUG_IC"
+RUN="./"
+MDPATH="./../"
+ICPATH="./../"
 
 # run make disk galaxy with input
-./../MakeDiskGalaxy.exe $MDGINPUT > "MakeDiskGalaxy.out"
+$MDPATH/MakeDiskGalaxy.exe $MDGINPUT > "MakeDiskGalaxy.out"
 
 # extract information from files
-./../loadIC.exe "ics_$MDGNAME.dat" > "loadIC.out"
+$ICPATH/loadIC.exe "ics_$MDGNAME.dat" > "loadIC.out"
 
 # write vcirc
 python "$RUN/extract_vcirc.py"
